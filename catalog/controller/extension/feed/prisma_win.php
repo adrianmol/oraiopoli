14d41cb592242e03a135e6d77c1833220156595f
Adrian Mold <addi.jeff@gmail.com>
2020-10-01 14:57:38 +0300

    prisma

--- catalog/controller/extension/feed/prisma_win.php
+++ catalog/controller/extension/feed/prisma_win.php
@@ -2,8 +2,11 @@
 class ControllerExtensionfeedPrismawin extends Controller {
 	public function index() {
 
+
+		$data = $this->GetProducts();
+
 		echo "<pre>";
-		print_r($this->GetProducts());
+			print_r($data);
 		echo "</pre>";
 
 
@@ -19,19 +22,19 @@ class ControllerExtensionfeedPrismawin extends Controller {
 
 		foreach($this->GetDataURL('GetProducts')->StoreDetails as $product){
 			
-			$data["id"]		    	=(string) $product->ItemId;
-			$data[$data["id"]]["code"]		=(string) $product->ItemCode;
-			$data[$data["id"]]["title"]		=(string) $product->ItemDescr;
-			$data[$data["id"]]["weight"]		=(string) $product->ItemWeight;
-			$data[$data["id"]]["itemStock"]	=(string) $product->ItemStock;
-			$data[$data["id"]]["category"]		=(string) $product->ItemGroup1;
-			$data[$data["id"]]["category_1"]		=(string) $product->ItemGroup2;
-			$data[$data["id"]]["category_2"]		=(string) $product->ItemGroup3;
-			$data[$data["id"]]["category_3"]		=(string) $product->ItemGroup4;
-			$data[$data["id"]]["manufacturer"]	=(string) $product->ItemManufacturer;
-			$data[$data["id"]]["manufacturer_id"]	=(string) $product->ItemManufacturerId;
-			$data[$data["id"]]["price_wholesale"]	=(string) $product->ItemWholesale;
-			$data[$data["id"]]["price_vat"]			=(string) $product->ItemRetailVat;
+			$data[$i]["id"]     	=(string) $product->ItemId;
+			$data[$i]["code"]		=(string) $product->ItemCode;
+			$data[$i]["title"]		=(string) $product->ItemDescr;
+			$data[$i]["weight"]		=(string) $product->ItemWeight;
+			$data[$i]["itemStock"]	=(string) $product->ItemStock;
+			$data[$i]["category"]		=(string) $product->ItemGroup1;
+			$data[$i]["category_1"]		=(string) $product->ItemGroup2;
+			$data[$i]["category_2"]		=(string) $product->ItemGroup3;
+			$data[$i]["category_3"]		=(string) $product->ItemGroup4;
+			$data[$i]["manufacturer"]	=(string) $product->ItemManufacturer;
+			$data[$i]["manufacturer_id"]	=(string) $product->ItemManufacturerId;
+			$data[$i]["price_wholesale"]	=(string) $product->ItemWholesale;
+			$data[$i]["price_vat"]			=(string) $product->ItemRetailVat;
 
 			$i++;
 		}
