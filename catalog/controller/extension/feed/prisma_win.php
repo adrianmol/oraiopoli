1b3962ccd4dbb885384ec789e1aafad4afac902d
Adrian Mold <addi.jeff@gmail.com>
2020-10-01 11:34:47 +0300

    PrismaWin

--- catalog/controller/extension/feed/prisma_win.php
+++ catalog/controller/extension/feed/prisma_win.php
@@ -2,13 +2,37 @@
 class ControllerExtensionfeedPrismawin extends Controller {
 	public function index() {
 
-			$url = "http://ecommercews.megasoft.gr/eCommerceWebService.asmx/GetProducts";
-	
-	
-				$xml = @simplexml_load_file($url);
-				echo "<pre>";
-				print_r($http_response_header);
-				echo "</pre>";
+		$originalXML = "
+		POST /eCommerceWebService.asmx HTTP/1.1
+		Host: ecommercews.megasoft.gr
+		Content-Type: text/xml; charset=utf-8
+		Content-Length: length
+		SOAPAction: 'http://tempuri.org/GetProducts'
+		
+		<?xml version='1.0' encoding='utf-8'?>
+		<soap:Envelope xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns:xsd='http://www.w3.org/2001/XMLSchema' xmlns:soap='http://schemas.xmlsoap.org/soap/envelope/'>
+		  <soap:Body>
+			<GetProducts xmlns='http://tempuri.org/'>
+			  <SiteKey></SiteKey>
+			  <Date></Date>
+			  <StorageCode>001</StorageCode>
+			</GetProducts>
+		  </soap:Body>
+		</soap:Envelope>";
+		
+
+
+		$someURL = "http://ecommercews.megasoft.gr/eCommerceWebService.asmx?op=GetProducts";
+		//Translate the XML above in a array, like PHP SOAP function requires
+		$myParams = array('firstClient' => array('SiteKey' => 'bs-gg183-352',
+										  'Date' => '1/9/2020'),
+										  'StorageCode' => '001');
+					
+		$webService = new SoapClient($someURL);
+		$result = $webService->someWebServiceFunction($myParams);
+
+		print_r ($result);
+		echo ($result);
 	}
 
 
