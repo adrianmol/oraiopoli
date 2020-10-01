e5d6019f7c2e9192709b945d1247fd689e2baf43
Adrian Mold <addi.jeff@gmail.com>
2020-10-01 14:39:28 +0300

    prisma

--- catalog/controller/extension/feed/prisma_win.php
+++ catalog/controller/extension/feed/prisma_win.php
@@ -19,19 +19,19 @@ class ControllerExtensionfeedPrismawin extends Controller {
 
 		foreach($this->GetDataURL('GetProducts')->StoreDetails as $product){
 			
-			$data["id"]    =(string) $product->ItemId;
-			$data["id"]["code"]		=(string) $product->ItemCode;
-			$data["id"]["title"]		=(string) $product->ItemDescr;
-			$data["id"]["weight"]		=(string) $product->ItemWeight;
-			$data["id"]["itemStock"]	=(string) $product->ItemStock;
-			$data["id"]["category"]		=(string) $product->ItemGroup1;
-			$data["id"]["category_1"]		=(string) $product->ItemGroup2;
-			$data["id"]["category_2"]		=(string) $product->ItemGroup3;
-			$data["id"]["category_3"]		=(string) $product->ItemGroup4;
-			$data["id"]["manufacturer"]	=(string) $product->ItemManufacturer;
-			$data["id"]["manufacturer_id"]	=(string) $product->ItemManufacturerId;
-			$data["id"]["price_wholesale"]	=(string) $product->ItemWholesale;
-			$data["id"]["price_vat"]			=(string) $product->ItemRetailVat;
+			$data[$i]["id"]     	=(string) $product->ItemId;
+			$data[$i]["code"]		=(string) $product->ItemCode;
+			$data[$i]["title"]		=(string) $product->ItemDescr;
+			$data[$i]["weight"]		=(string) $product->ItemWeight;
+			$data[$i]["itemStock"]	=(string) $product->ItemStock;
+			$data[$i]["category"]		=(string) $product->ItemGroup1;
+			$data[$i]["category_1"]		=(string) $product->ItemGroup2;
+			$data[$i]["category_2"]		=(string) $product->ItemGroup3;
+			$data[$i]["category_3"]		=(string) $product->ItemGroup4;
+			$data[$i]["manufacturer"]	=(string) $product->ItemManufacturer;
+			$data[$i]["manufacturer_id"]	=(string) $product->ItemManufacturerId;
+			$data[$i]["price_wholesale"]	=(string) $product->ItemWholesale;
+			$data[$i]["price_vat"]			=(string) $product->ItemRetailVat;
 
 			$i++;
 		}
