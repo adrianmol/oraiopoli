2371557431bff29866d34197d723d62c9b8d832e
Adrian Mold <addi.jeff@gmail.com>
2020-10-01 11:17:26 +0300

    PrismaWin

--- catalog/controller/extension/feed/prisma_win.php
+++ catalog/controller/extension/feed/prisma_win.php
@@ -2,25 +2,11 @@
 class ControllerExtensionfeedPrismawin extends Controller {
 	public function index() {
 
-	
-
-
-
-
-
-
-
-
-		function GetProducts() {
-
 			$url = "http://ecommercews.megasoft.gr/eCommerceWebService.asmx/GetProducts";
 	
 	
 				$xml = @simplexml_load_file($url);
 				print_r($http_response_header);
-	
-	
-		}
 
 	}
 
