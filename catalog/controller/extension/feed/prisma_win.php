c8e8621031b493f7701ef3e1f2fe97d88243870b
Adrian Mold <addi.jeff@gmail.com>
2020-10-01 12:24:02 +0300

    prisma

--- catalog/controller/extension/feed/prisma_win.php
+++ catalog/controller/extension/feed/prisma_win.php
@@ -2,14 +2,16 @@
 class ControllerExtensionfeedPrismawin extends Controller {
 	public function index() {
 
-		$url = 'ecommercews.megasoft.gr';
+		$url = 'ecommercews.megasoft.gr/eCommerceWebService.asmx/GetProducts';
 		$data = 'SiteKey=bs-gg183-352&Date=1-9-2020&StorageCode=000';
 		
 		// use key 'http' even if you send the request to https://...
 		$options = array(
 			'http' => array(
 				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-				'method'  => 'POST /eCommerceWebService.asmx/GetProducts',
+				'host'    => "ecommercews.megasoft.gr",	
+				'Content-Length: ' => "length",
+				'method'  => 'POST',
 				'content' => http_build_query($data)
 			)
 		);
