01b2f4e886b730f0326e394077fb6fff4f97b08a
Adrian Mold <addi.jeff@gmail.com>
2020-10-06 13:51:43 +0300

    Product Q

--- catalog/controller/extension/feed/prisma_win.php
+++ catalog/controller/extension/feed/prisma_win.php
@@ -159,7 +159,7 @@ class ControllerExtensionfeedPrismawin extends Controller {
 							product_id  = '".(int)$products[29039]['id']."' ON DUPLICATE KEY UPDATE product_id = '".(int)$products[29039]['id']."',
 							language_id = '".$language_id."',
 							name = '".(string)$products[29039]['title'] . "',
-							meta_title = '".(string)$products[29039]['title'] . "',
+							meta_title = '".(string)$products[29039]['title'] . "'
 
 							");	
 
@@ -172,7 +172,7 @@ class ControllerExtensionfeedPrismawin extends Controller {
 
 		$insertproduct = $this->db->query("INSERT INTO ". DB_PREFIX ."product_to_category  SET 
 							product_id  = '".(int)$products[29039]['id']."' ON DUPLICATE KEY UPDATE product_id = '".(int)$products[29039]['id']."',
-							category_id = '".(int)$categories[29039]['categoryID']."' ON DUPLICATE KEY UPDATE product_id = '".(int)$categories[29039]['categoryID']."',
+							category_id = '".(int)$categories[29039]['categoryID']."' ON DUPLICATE KEY UPDATE product_id = '".(int)$categories[29039]['categoryID']."'
 
 							");	
 
