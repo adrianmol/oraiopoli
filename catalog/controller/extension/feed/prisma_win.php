1083f62891a4088b2df1ac0e202919ab77d82166
Adrian Mold <addi.jeff@gmail.com>
2020-10-09 16:15:16 +0300

    Products AAA

--- catalog/controller/extension/feed/prisma_win.php
+++ catalog/controller/extension/feed/prisma_win.php
@@ -131,14 +131,14 @@ class ControllerExtensionfeedPrismawin extends Controller {
 		$products   = $this->GetProducts();
 		$products = $products[0];
 		
-		foreach($products as $product){
+		// foreach($products as $product){
 
-		$filename = "http://www.oraiomarket.gr/image/catalog/products/00004616.JPG";
-		if(file_exists($filename))		
+		$filename = "/image/catalog/products/00000018.JPG";
+		if(file_exists($filename))	{	
 		// $this->GetPhotoPath($product['code']);
-				echo ($product['code']);
-				echo "</br>";
-		}
+				// echo ($product['code']);
+				echo "TEST</br>";}
+		// }
 
 	}
 
