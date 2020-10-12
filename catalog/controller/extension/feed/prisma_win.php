171112c18fb40692ceafd758bb7d84d2df1891e2
Adrian Mold <addi.jeff@gmail.com>
2020-10-12 11:03:04 +0300

    Photo Path

--- catalog/controller/extension/feed/prisma_win.php
+++ catalog/controller/extension/feed/prisma_win.php
@@ -352,23 +352,23 @@ class ControllerExtensionfeedPrismawin extends Controller {
 		
 		echo "</br>";
 		echo ($data);
-		// use key 'http' even if you send the request to https://...
-		// $options = array(
-		// 	'http' => array(
-		// 		'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-		// 		'method'  => 'POST',
-		// 		'content' => $data
-		// 	)
-		// );
-		// $context  = stream_context_create($options);
-		// $result = file_get_contents($url, false, $context);
-		// if ($result === FALSE) { /* Handle error */ }
-		// //echo $result;
-		// $xml=simplexml_load_string($result) or die("Error: Cannot create object");
-		// // echo "<pre>";
-		// // print_r($xml);
-		// // echo "</pre>";
-		// return $result;
+		//use key 'http' even if you send the request to https://...
+		$options = array(
+			'http' => array(
+				'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+				'method'  => 'POST',
+				'content' => $data
+			)
+		);
+		$context  = stream_context_create($options);
+		$result = file_get_contents($url, false, $context);
+		if ($result === FALSE) { /* Handle error */ }
+		//echo $result;
+		$xml=simplexml_load_string($result) or die("Error: Cannot create object");
+		// echo "<pre>";
+		// print_r($xml);
+		// echo "</pre>";
+		return $result;
 
 	}
 
