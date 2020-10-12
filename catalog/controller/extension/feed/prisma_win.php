c7de6a76d06d57400303ea9a2eb1de7c4e5dfcdc
Adrian Mold <addi.jeff@gmail.com>
2020-10-12 11:59:59 +0300

    Product A

--- catalog/controller/extension/feed/prisma_win.php
+++ catalog/controller/extension/feed/prisma_win.php
@@ -366,9 +366,15 @@ class ControllerExtensionfeedPrismawin extends Controller {
 		if ($result === FALSE) { /* Handle error */ }
 		//echo $result;
 		$xml=simplexml_load_string($result) or die("Error: Cannot create object");
-		$xml1 = $xml->ItemImageUpload;
+		
+		foreach($xml->ItemImageUpload as $photoInfo){
+			$data[$photoInfo->ItemCode] = array(
+
+			'itemcode'  => $photoInfo->ItemCode,
+			'itemrtype' => $photoInfo->ItemType
+			)}
 		echo "<pre>";
-		print_r($xml1);
+		print_r($data);
 		echo "</pre>";
 		return $result;
 
