9303373937d7b284d5d11efa8d3a4e0d4ca2e402
Adrian Mold <addi.jeff@gmail.com>
2020-10-30 16:11:39 +0200

    Categories

--- catalog/controller/extension/feed/prisma_win.php
+++ catalog/controller/extension/feed/prisma_win.php
@@ -144,7 +144,7 @@ class ControllerExtensionfeedPrismawin extends Controller {
 
 			$productID = $category['productID'];
 
-			$parent_level = $level-1;
+			
 			$my_category = $this->db->query("select  s.product_id, cp.path_id, cp.`level`,z.`name` from oc_product s 
 			LEFT JOIN oc_product_description d ON(d.product_id= s.product_id and d.language_id=2)
 			LEFT JOIN oc_product_to_category r ON(r.product_id= s.product_id)
@@ -176,7 +176,7 @@ class ControllerExtensionfeedPrismawin extends Controller {
 		echo "<pre>";
 		print_r($CategoryPath);
 		echo "</pre>";
-		
+
 		return $CategoryPath;
 
 	}
