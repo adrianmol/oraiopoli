c80f7be27a2e13615c8741c3af2af70f8f959d6a
Adrian Mold <addi.jeff@gmail.com>
2020-10-30 13:46:10 +0200

    get category

--- catalog/controller/extension/feed/prisma_win.php
+++ catalog/controller/extension/feed/prisma_win.php
@@ -140,7 +140,7 @@ class ControllerExtensionfeedPrismawin extends Controller {
 
 
 
-			$categoryID = $this->db->query("SELECT cd.category_id FROM ". DB_PREFIX ."category cd ,". DB_PREFIX ."category c WHERE cd.name = '" . $category['level'.$level.''] . "'");
+			$categoryID = $this->db->query("SELECT cd.category_id FROM ". DB_PREFIX ."category_description cd ,". DB_PREFIX ."category c WHERE cd.name = '" . $category['level'.$level.''] . "'");
 			
 			$catID = $categoryID->rows[0];
 			// $CategoryPath[$category['ID']] =  array (
