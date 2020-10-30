f6ba45c0630fbff08cc8f6bb1715b0f8f536781f
Adrian Mold <addi.jeff@gmail.com>
2020-10-30 18:08:32 +0200

    categories

--- catalog/controller/extension/feed/prisma_win.php
+++ catalog/controller/extension/feed/prisma_win.php
@@ -157,7 +157,7 @@ class ControllerExtensionfeedPrismawin extends Controller {
 		foreach($data[1] as $category){
 		
 
-			echo ("Last key: ". (array_key_last($category))."<br>");
+			echo ("Last key: ". array_key_last($category)."<br>");
 
 			if(empty($category['level3'])){
 				if(empty($category['level2'])){
