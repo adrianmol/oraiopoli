e6c1d2d57441aaacdbf03e7cef18cec9584835a3
Adrian Mold <addi.jeff@gmail.com>
2020-10-30 16:13:24 +0200

    Categories

--- catalog/controller/extension/feed/prisma_win.php
+++ catalog/controller/extension/feed/prisma_win.php
@@ -139,7 +139,7 @@ class ControllerExtensionfeedPrismawin extends Controller {
 			}else if ($category['level1']){
 				$level = 1;
 				$main_category = $category['level1'];
-				$parent_category = "";
+				$parent_category = $category['top_category'];
 			}
 
 			$productID = $category['productID'];
