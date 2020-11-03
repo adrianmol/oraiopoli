432b44a1b71bd44b83448bf23427ad475bdc46ed
Adrian Mold <addi.jeff@gmail.com>
2020-11-03 15:51:40 +0200

    Orders

--- catalog/controller/extension/feed/prisma_win.php
+++ catalog/controller/extension/feed/prisma_win.php
@@ -611,7 +611,7 @@ class ControllerExtensionfeedPrismawin extends Controller {
 	function jsonRemoveUnicodeSequences($struct) {
 		try {
 			return preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/', function ($match) {
-				return mb_convert_encoding(pack('H*', $match[1]), 'utf8_general_ci', 'UCS-2BE');
+				return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UCS-2BE');
 			}, json_encode($struct));
 		
 		 } catch (Exception $e) {
